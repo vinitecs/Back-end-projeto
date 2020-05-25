2547b62a963fb35e2070c483bb9ebe4d7b7b3686
vinicius <viniciusamalia@gmail.com>
2020-05-24 21:55:33 -0300

    criando tratamneto de erros com ID

--- src/main/java/br/com/vini/projetointegrador/services/CategoriaService.java
+++ src/main/java/br/com/vini/projetointegrador/services/CategoriaService.java
@@ -3,10 +3,12 @@ package br.com.vini.projetointegrador.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.vini.projetointegrador.dominio.Categoria;
 import br.com.vini.projetointegrador.repository.CategoriaRepository;
+import br.com.vini.projetointegrador.services.exception.DataIntegrityException;
 import br.com.vini.projetointegrador.services.exception.ObjectNotFoundException;
 
 @Service
@@ -16,7 +18,7 @@ public class CategoriaService {
 
 	
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
@@ -25,5 +27,19 @@ public class CategoriaService {
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir cxaategoria que possui produto cadastrado");
+		}
 	}
 }
